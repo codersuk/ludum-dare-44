8c7bf848952b660af739ecebbce829871a3dc80c
Bhavesh Ramburn <nitrogen@gmail.com>
2019-04-27 15:49:02 +0100

    added double jump

--- components/player.js
+++ components/player.js
@@ -9,6 +9,20 @@ Crafty.c("Player", {
 		this.vx += MOVE_RIGHT_RATE_PLAYER;
 		this.onHit("BountyHunter", function (hitData) {
 			Crafty.trigger(PLAYER_HIT_BOUNTY_HUNTER_EVENT)
-		})
+		});
+		this.hasDoubleJumpPowerUp = true;
+		//double jump check
+		//Use default template online
+		this.bind("CheckJumping", function(ground) {
+			if (!ground && this.hasDoubleJumpPowerUp) {
+				this.canJump = true;
+				this.jumpSpeed(350);
+				this.hasDoubleJumpPowerUp = false;
+			}
+		});
+		this.bind("LandedOnGround", function(ground) {
+			this.jumpSpeed(300);
+			this.hasDoubleJumpPowerUp = true; // give this new double jump powerup upon landing
+		});
 	},
 });
