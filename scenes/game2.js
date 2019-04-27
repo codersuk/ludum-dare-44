a5e4bcb385c9c4294524d4aff6e2c19ab891bbc7
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-27 11:59:22 +0100

    added twoway movement to confirm camera tracking works.

--- scenes/game2.js
+++ scenes/game2.js
@@ -7,11 +7,12 @@ Crafty.defineScene("Game2", function() {
 		.attr({x: 0, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT, w: 3000, h: SINGLE_UNIT})
 		.color('#040');
 
-	var player = Crafty.e('2D, DOM, Color, twoway')
+	var player = Crafty.e('2D, DOM, Color, Twoway')
 		.attr({x: GAME_SCREEN_WIDTH / 2, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT - SINGLE_UNIT, w: DOUBLE_UNIT, h: SINGLE_UNIT})
-		.color('#171');
+		.twoway(200)
+		.color('#171')
 
-	var bountyhunter = Crafty.e('2D, DOM, Color, twoway')
+	var bountyhunter = Crafty.e('2D, DOM, Color')
 		.attr({x: GAME_SCREEN_WIDTH / 25, y: GAME_SCREEN_HEIGHT - DOUBLE_UNIT - SINGLE_UNIT, w: SINGLE_UNIT, h: DOUBLE_UNIT})
 		.color('#111');
 
