3486a12e9984c8a115f330df5457a86d8c100dc2
Sumia <sumia.017@gmail.com>
2019-04-27 19:15:22 +0100

    changed log width

--- components/obstacles/obstacle_woodenlog.js
+++ components/obstacles/obstacle_woodenlog.js
@@ -6,7 +6,7 @@ Crafty.c(WOODENLOG, {
 		this.requires('2D, DOM, Color, Obstacle')
         this.attr({x: (GAME_SCREEN_WIDTH/2 + player.x + 100),
              y: GAME_SCREEN_HEIGHT - SINGLE_UNIT - woodenlog_height, 
-             w: DOUBLE_UNIT / 10, 
+             w: SINGLE_UNIT, 
              h: woodenlog_height})
 		this.color('#654321');
 	},
