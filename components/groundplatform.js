a54707728548ce398e6bccfa4a16c10ead9a5e91
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-27 19:13:56 +0100

    The player can land on stone and log.

---
+++ components/groundplatform.js
@@ -0,0 +1,8 @@
+Crafty.c("GroundPlatform", {
+	init : function () {
+		this.requires('Platform, Image')
+		this.attr({x: 0, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT, w: 3000, h: SINGLE_UNIT})
+		this.image("images/sprites/grass.png","repeat-x")
+	},
+});
+
