6ea8e7e2e6e5825f5de0889b5fb9840a956c6705
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-27 12:18:15 +0100

    Added trigger for player hit bounty.

--- scenes/game2.js
+++ scenes/game2.js
@@ -1,6 +1,8 @@
 var SINGLE_UNIT = 64;
 var DOUBLE_UNIT = 128;
 
+var PLAYER_HIT_BOUNTY_HUNTER_EVENT = "PLAYER_HIT_BOUNTY_HUNTER_EVENT";
+
 function makeCameraTrackEntity(entity, yoffset) {
 	Crafty.viewport.clampToEntities = false;
 	Crafty.viewport.scale(1);
@@ -11,20 +13,26 @@ function makeCameraTrackEntity(entity, yoffset) {
 }
 
 Crafty.defineScene("Game2", function() {
-	
 	var platform = Crafty.e('2D, DOM, Color')
 		.attr({x: 0, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT, w: 3000, h: SINGLE_UNIT})
 		.color('#040');
 
-	var player = Crafty.e('2D, DOM, Color, Twoway')
+	var player = Crafty.e('2D, DOM, Color, Twoway, Player, Collision')
 		.attr({x: GAME_SCREEN_WIDTH / 2, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT - SINGLE_UNIT, w: DOUBLE_UNIT, h: SINGLE_UNIT})
 		.twoway(200)
 		.color('#171')
+		.onHit("BountyHunter", function (hitData) {
+			Crafty.trigger(PLAYER_HIT_BOUNTY_HUNTER_EVENT)
+		})
 
-	var bountyhunter = Crafty.e('2D, DOM, Color')
+	var bountyhunter = Crafty.e('BountyHunter, 2D, DOM, Color, Collision')
 		.attr({x: GAME_SCREEN_WIDTH / 25, y: GAME_SCREEN_HEIGHT - DOUBLE_UNIT - SINGLE_UNIT, w: SINGLE_UNIT, h: DOUBLE_UNIT})
 		.color('#111');
 
 	// TODO Offset calculation isn't right, close enough for now.
 	makeCameraTrackEntity(player, DOUBLE_UNIT  + SINGLE_UNIT)
 });
+
+Crafty.bind(PLAYER_HIT_BOUNTY_HUNTER_EVENT, function () {
+	Crafty.scene("Game2")
+})
