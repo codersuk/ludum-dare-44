8a09b6ba56be7a6e43aae6302fe3c91624bb0444
Bhavesh Ramburn <nitrogen@gmail.com>
2019-04-27 14:24:51 +0100

    Update player.js

--- components/player.js
+++ components/player.js
@@ -2,7 +2,7 @@ Crafty.c("Player", {
 	//TODO: define the size of the Player 64x64 or 128x64?
 	init : function () {
 
-		this.requires('2D, DOM, Collision, Motion, Gravity, Jumper player')
+		this.requires('2D, DOM, Collision, Motion, Gravity, Jumper, player')
 		this.attr({x: GAME_SCREEN_WIDTH / 2, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT - SINGLE_UNIT, w: DOUBLE_UNIT, h: SINGLE_UNIT})
 		this.jumper(300, ['UP_ARROW', 'W']);
   		this.gravity("Platform");
