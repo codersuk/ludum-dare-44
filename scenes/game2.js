9208c2522744bf4c83055e1cb73c1c6cd5da6b5c
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-27 12:10:41 +0100

    tracks player, close enough for now

--- scenes/game2.js
+++ scenes/game2.js
@@ -1,6 +1,15 @@
 var SINGLE_UNIT = 64;
 var DOUBLE_UNIT = 128;
 
+function makeCameraTrackEntity(entity, yoffset) {
+	Crafty.viewport.clampToEntities = false;
+	Crafty.viewport.scale(1);
+	Crafty.one("CameraAnimationDone", function() {
+	    Crafty.viewport.follow(entity, 0,  0 + yoffset);
+	});
+	Crafty.viewport.centerOn(entity, 10);
+}
+
 Crafty.defineScene("Game2", function() {
 	
 	var platform = Crafty.e('2D, DOM, Color')
@@ -16,4 +25,6 @@ Crafty.defineScene("Game2", function() {
 		.attr({x: GAME_SCREEN_WIDTH / 25, y: GAME_SCREEN_HEIGHT - DOUBLE_UNIT - SINGLE_UNIT, w: SINGLE_UNIT, h: DOUBLE_UNIT})
 		.color('#111');
 
+	// TODO Offset calculation isn't right, close enough for now.
+	makeCameraTrackEntity(player, DOUBLE_UNIT  + SINGLE_UNIT)
 });
