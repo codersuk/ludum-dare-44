8b2d2bdc70ea2ed82897a93706c13c1a117679d4
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-27 16:49:38 +0100

    fixed position for countdown text. Unselectable and bigger font.

--- components/countdown.js
+++ components/countdown.js
@@ -1,9 +1,12 @@
 Crafty.c("Countdown", {
     init : function () {
-        this.requires('2D, DOM, Text')
+        this.requires('2D, DOM, Text, HUD')
         this.totalSeconds = 2 * 60;
         this.attr({w: 50, h: 100, x: 700, y: 20})
+        this.fixedPosition(700, 20)
         this.text("02:00")
+        this.unselectable()
+        this.textFont({ size: '24px', weight: 'bold' });
         this.bind("UpdateTime", function () {
             //calculate time
             this.totalSeconds -= 1;
