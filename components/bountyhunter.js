ef8009d0e930d5fe1dfc3641dc3495fb57828d63
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-28 11:39:01 +0100

    Reset velocity to previous valid when you start moving - small permantant penalty.

--- components/bountyhunter.js
+++ components/bountyhunter.js
@@ -3,7 +3,7 @@ Crafty.c("BountyHunter", {
 		this.requires('2D, DOM, Collision, Motion, bountyHunter')
 		this.attr({x: GAME_SCREEN_WIDTH / 25, y: GAME_SCREEN_HEIGHT - 192 - SINGLE_UNIT, w: 96, h: 192})
 		this.vx += MOVE_RIGHT_RATE_BOUNTY_HUNTER;
-		this.ax += MOVE_RIGHT__ACCELERATION_RATE_PLAYER;
+		this.ax += MOVE_RIGHT_ACCELERATION_RATE_PLAYER;
 	},
 });
 
