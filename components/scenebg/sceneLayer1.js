5d5c2e52ad1b66adaab9c98a40c43a7d47bcd2cd
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-28 14:54:54 +0100

    add the normal png back for now.

--- components/scenebg/sceneLayer1.js
+++ components/scenebg/sceneLayer1.js
@@ -5,7 +5,6 @@ Crafty.c("sceneLayer1", {
         this.requires('2D, DOM, Image,Motion');
         //1300 is the width of the image
 
-
         this.attr({x: 0, y: GAME_SCREEN_HEIGHT - heightOfImage, w: widthOfImage * 100, h: heightOfImage});
         this.image("images/background/BG4.png", "repeat-x");
         // this.vx -= 50;
@@ -14,9 +13,7 @@ Crafty.c("sceneLayer1", {
     },
     events: {
         "UpdateFrame": function () {
-
             //repeat screen until the end of time
-            console.log(-widthOfImage);
             if (this.x <= -(widthOfImage)) {
                 this.x = 0;
                 console.log('setup backgou')
