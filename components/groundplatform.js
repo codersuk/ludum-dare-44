697915db23968acb0f8ff810cfcfc6055d080f9d
Steven Rutherford <steve.rutherford12@gmail.com>
2019-04-28 10:48:13 +0100

    Made platform and background repeat more.

--- components/groundplatform.js
+++ components/groundplatform.js
@@ -1,7 +1,7 @@
 Crafty.c("GroundPlatform", {
 	init : function () {
 		this.requires('Platform, Image')
-		this.attr({x: 0, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT, w: 3000, h: SINGLE_UNIT})
+		this.attr({x: 0, y: GAME_SCREEN_HEIGHT - SINGLE_UNIT, w: 300000, h: SINGLE_UNIT})
 		this.image("images/sprites/grass.png","repeat-x")
 	},
 });
