5e4e7377dfa565ee084071c9e1ff612d4acd9285
Steven Rutherford <steve.rutherford12@gmail.com>
2019-05-01 09:23:39 +0100

    FIx comma bug.

--- components/obstacles/obstacle_woodenlog.js
+++ components/obstacles/obstacle_woodenlog.js
@@ -5,7 +5,7 @@ Crafty.c(WOODENLOG, {
 	init : function () {
 		this.requires('2D, DOM, Obstacle, Platform, woodenlog')
         this.attr({x: (GAME_SCREEN_WIDTH + GhostPlayer.x),
-             y: GAME_SCREEN_HEIGHT - DISTANCE_UP_FROM_BOTTOM_OF_SCREEN - 10 - woodenlog_height,  - (Math.random() * 10 - 10),
+             y: GAME_SCREEN_HEIGHT - DISTANCE_UP_FROM_BOTTOM_OF_SCREEN - 10 - woodenlog_height  - (Math.random() * 10 - 10),
              w: woodenlog_width, 
              h: woodenlog_height})
         this.collision([0, 5, this.w, 5, this.w, this.h, 0, this.h])
